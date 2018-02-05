8ffd4eabf479764a27ff3966eb5223b3d7d5e801
gabrielrond777 <gabrielrond777@gmail.com>
2018-02-05 15:48:32 +0700

    new comm

---
+++ exercises9.js
@@ -0,0 +1,25 @@
+Tugas 1
+
+function shoutOut(){
+ console.log('Halo Function!');
+} shoutOut();
+
+Tugas 2
+
+var num1 = 5;
+var num2 = 6;
+
+ function calculateMultiply () {
+   console.log(num1 * num2);
+ }calculateMultiply();
+
+ Tugas 3
+
+var nama = 'gabriel';
+var age = 19;
+var address = 'ciputat ';
+var hobby = 'gaming';
+
+function fullSentence(){
+console.log('Nama saya ' +nama+',Umur saya '+age+',Alamat saya '+address+',hobby saya '+hobby );
+} fullSentence();
