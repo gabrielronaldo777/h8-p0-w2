3b05142b5bfdc39625460c665db8c1c26af0e031
gabrielrond777 <gabrielrond777@gmail.com>
2018-02-20 14:49:52 +0700

    barru

--- exercises4.js
+++ exercises4.js
@@ -1,29 +1,21 @@
-var hari = '17'; //1-31
-var bulan = '2';//januari - desember
-var tahun = '2018'; //1900-2200
-for (hari;hari > 0 && hari <= 31; hari++ ){
-    console.log(hari);
-  break;
+var tanggal = 1;
+var bulan = 1;
+var tahun = 1945 ;
+
+switch(bulan){
+  case 1 : {bulan='Januari';break;}
+  case 2 : {bulan='februari'; break;}
+  case 3 : {bulan='maret'; break;}
+  case 4 : {bulan='april'; break;}
+  case 5 : {bulan='mei'; break;}
+  case 6 : {bulan='juni'; break;}
+  case 7 : {bulan='juli'; break;}
+  case 8 : {bulan='agustus'; break;}
+  case 9 : {bulan='september'; break;}
+  case 10: {bulan='oktober'; break;}
+  case 11: {bulan='november'; break;}
+  default : {bulan='desember'; break;}
 }
 
+console.log(tanggal+' '+ bulan + ' ' + tahun);
 
- switch(bulan){
-   case '1':   { console.log('januari'); break; }
-   case '2':   { console.log('februari'); break; }
-   case '3':   { console.log('maret'); break; }
-   case '4':   { console.log('april'); break; }
-   case '5':   { console.log('mei'); break; }
-   case '6':   { console.log('juni'); break; }
-   case '7':   { console.log('juli'); break; }
-   case '8':   { console.log('agustus'); break; }
-   case '9':   { console.log('september'); break; }
-   case '10':   { console.log('oktober'); break; }
-   case '11':   { console.log('november'); break; }
-   case '12':   { console.log('desember'); break; }
-
-    default:  { console.log('bukan nama bulan'); }
-}
-for (tahun; tahun > 1900 && tahun <= 2200; tahun++ ){
-  console.log(tahun);
-  break;
-}
